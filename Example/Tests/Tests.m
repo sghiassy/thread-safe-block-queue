f750a531624f266b6848131b34bb2ca53ac5c5fd
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2016-06-28 15:59:03 -0700

    Add Replay Ability to thread-safe-block-queue

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -13,7 +13,7 @@
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will pass", ^{
+describe(@"ThreadSafeBlockQueue", ^{
 
     __block ThreadSafeBlockQueue *queue;
 
@@ -93,34 +93,267 @@ describe(@"these will pass", ^{
     });
 
     it(@"will run blocks immediatly after the queue has been enqueued", ^{
-        __block CGFloat count = 0.0f;
+        waitUntil(^(DoneCallback done) {
+            __block CGFloat count = 0.0f;
 
-        [queue queueBlock:^{
-            expect(count).to.equal(0.0f);
-            count += 2.2f;
-        }];
+            [queue queueBlock:^{
+                expect(count).to.equal(0.0f);
+                count += 2.2f;
+            }];
 
-        [queue queueBlock:^{
-            expect(count).to.equal(2.2f);
-            count += 3.3f;
-        }];
+            [queue queueBlock:^{
+                expect(count).to.equal(2.2f);
+                count += 3.3f;
+            }];
 
-        [queue enQueueAllBlocksAndRun];
+            [queue enQueueAllBlocksAndRun];
 
-        [queue queueBlock:^{
-            expect(count).to.equal(5.5f);
-            count += 4.4f;
-        }];
+            [queue queueBlock:^{
+                expect(count).to.equal(5.5f);
+                count += 4.4f;
+            }];
 
-        [queue queueBlock:^{
-            expect(count).to.equal(9.9f);
-            count += 5.5f;
-        }];
+            [queue queueBlock:^{
+                expect(count).to.equal(9.9f);
+                count += 5.5f;
+            }];
+            
+            [queue queueBlock:^{
+                expect(count).to.equal(15.4f);
+                done();
+            }];
+        });
+    });
 
-        [queue queueBlock:^{
-            expect(count).to.equal(15.4f);
-        }];
+    it(@"can replay the order of the queue", ^{
+        waitUntil(^(DoneCallback done) {
+            __block CGFloat count = -1.0f;
+            __block NSUInteger called = 0;
 
+            [queue queueBlock:^{
+                called++; // log the fact that we were called
+                count = 0.0f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(0.0f);
+                count += 2.2f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(2.2f);
+                count += 3.3f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(5.5f);
+                count += 4.4f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(9.9f);
+                count += 5.5f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(15.4f);
+            }];
+
+            expect(queue.currentState).to.equal(ThreadSafeBlockQueueStopped);
+
+            [queue enQueueAllBlocksAndRunOnComplete:^{
+                expect(called).to.equal(1);
+                expect(count).to.equal(15.4f);
+                expect(queue.currentState).to.equal(ThreadSafeBlockQueueRunning);
+                [queue replay];
+                [queue queueBlock:^{
+                    expect(count).to.equal(15.4f);
+                    count += 6.6f;
+                    expect(count).to.equal(22.0f);
+                    
+                    expect(called).to.equal(2);
+
+                    done();
+                }];
+            }];
+        });
+
+    });
+
+    it(@"calling replay multiple times is fine", ^{
+
+        waitUntil(^(DoneCallback done) {
+            // Async example blocks need to invoke done() callback.
+            __block CGFloat count = -1.0f;
+            __block NSUInteger called = 0;
+
+            [queue queueBlock:^{
+                called++; // log the fact that we were called
+                count = 0.0f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(0.0f);
+                count += 2.2f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(2.2f);
+                count += 3.3f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(5.5f);
+                count += 4.4f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(9.9f);
+                count += 5.5f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(15.4f);
+            }];
+
+            [queue enQueueAllBlocksAndRunOnComplete:^{
+                expect(called).to.equal(1);
+                expect(count).to.equal(15.4f);
+                [queue replay];
+                [queue replay];
+                [queue replay];
+                [queue replay];
+                [queue replay];
+                [queue queueBlock:^{
+                    expect(count).to.equal(15.4f);
+                    count += 6.6f;
+                    expect(count).to.equal(22.0f);
+                    
+                    expect(called).to.equal(6);
+                }];
+                done();
+            }];
+        });
+
+    });
+
+    it(@"can take a block that is run only once. Regardless of replay being called on it", ^{
+
+        waitUntil(^(DoneCallback done) {
+            // Async example blocks need to invoke done() callback.
+            __block CGFloat count = -1.0f;
+            __block CGFloat called = 0.0f;
+
+            [queue queueBlock:^{
+                called += 1.0f; // log the fact that we were called
+                count = 0.0f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(0.0f);
+                count += 2.2f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(2.2f);
+                count += 3.3f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(5.5f);
+                count += 4.4f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(9.9f);
+                count += 5.5f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(15.4f);
+            }];
+
+            [queue enQueueAllBlocksAndRunOnComplete:^{
+                expect(called).to.equal(1);
+                expect(count).to.equal(15.4f);
+                [queue replay];
+                [queue replay];
+                [queue queueBlock:^{
+                    called += 0.1f;
+                } shouldReplay:NO];
+                [queue replay];
+                [queue replay];
+                [queue replay];
+                [queue queueBlock:^{
+                    expect(count).to.equal(15.4f);
+                    count += 6.6f;
+                    expect(count).to.equal(22.0f);
+
+                    expect(called).to.equal(6.1f); // Has the single run block be run multiple times the decimal would be off here
+                }];
+                done();
+            }];
+        });
+    });
+
+    it(@"can handle multi-threads during restart", ^{
+        waitUntil(^(DoneCallback done) {
+            dispatch_queue_t concurrentQueue = dispatch_queue_create("com.Groupon.ThreadSafeBlockQueue2", DISPATCH_QUEUE_CONCURRENT);
+            dispatch_suspend(concurrentQueue);
+
+            __block CGFloat count = -1.0f;
+            __block CGFloat called = 0.0f;
+
+            [queue queueBlock:^{
+                called += 1.0f; // log the fact that we were called
+                count = 0.0f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(0.0f);
+                count += 2.2f;
+            }];
+
+            [queue queueBlock:^{
+                expect(count).to.equal(2.2f);
+            }];
+
+            [queue enQueueAllBlocksAndRunOnComplete:^{
+                dispatch_async(concurrentQueue, ^{
+                    [queue enQueueAllBlocksAndRunOnComplete:^{
+                        expect(count).to.equal(2.2f);
+                        done();
+                    }];
+                });
+
+                dispatch_async(concurrentQueue, ^{
+                    [queue enQueueAllBlocksAndRunOnComplete:^{
+                        expect(count).to.equal(2.2f);
+                        done();
+                    }];
+                });
+                
+                dispatch_async(concurrentQueue, ^{
+                    expect(count).to.equal(2.2f);
+                    [queue replay];
+                });
+
+                dispatch_async(concurrentQueue, ^{
+                    [queue enQueueAllBlocksAndRunOnComplete:^{
+                        expect(count).to.equal(2.2f);
+                    }];
+                });
+
+                dispatch_async(concurrentQueue, ^{
+                    [queue enQueueAllBlocksAndRunOnComplete:^{
+                        expect(count).to.equal(2.2f);
+                        done();
+                    }];
+                });
+
+                dispatch_resume(concurrentQueue);
+            }];
+        });
     });
 });
 
--- thread-safe-block-queue/Classes/ThreadSafeBlockQueue.m
+++ thread-safe-block-queue/Classes/ThreadSafeBlockQueue.m
@@ -10,8 +10,10 @@
 
 @interface ThreadSafeBlockQueue ()
 
-@property (atomic, strong) dispatch_queue_t serialQueue; // the queue that all operations are run on inside this datastructure
-@property (atomic, assign) BOOL isSuspended;
+@property (nonatomic, strong) dispatch_queue_t serialQueue; // the queue that all operations are run on inside this datastructure
+@property (atomic, readwrite, assign) ThreadSafeBlockQueueStates currentState;
+
+@property (nonatomic, strong) NSMutableArray *blocks;
 
 @end
 
@@ -24,8 +26,11 @@
     self = [super init];
 
     if (self) {
-        // Create and suspend the queue
+        // Instantiate Objects
+        _currentState = ThreadSafeBlockQueueRunning;
         _serialQueue = dispatch_queue_create("com.Groupon.ThreadSafeBlockQueue", DISPATCH_QUEUE_SERIAL);
+        _blocks = [[NSMutableArray alloc] init];
+
         [self suspendQueue];
     }
 
@@ -33,8 +38,10 @@
 }
 
 - (void)dealloc {
-    if (self.isSuspended == YES) {
-        // You can't dealloc a suspended queue: http://stackoverflow.com/a/9572316/1179897
+    BOOL isStopped = self.currentState == ThreadSafeBlockQueueStopped;
+
+    // You can't dealloc a suspended queue: http://stackoverflow.com/a/9572316/1179897
+    if (isStopped) {
         [self startQueue];
     }
 
@@ -44,37 +51,114 @@
 #pragma mark - Start / Stop Methods
 
 - (void)startQueue {
+    BOOL alreadyRunning = self.currentState == ThreadSafeBlockQueueRunning;
+
+    if (alreadyRunning) {
+        return;
+    }
+
     dispatch_resume(_serialQueue);
-    _isSuspended = NO;
+    self.currentState = ThreadSafeBlockQueueRunning;
 }
 
 - (void)suspendQueue {
+    BOOL alreadySuspended = self.currentState == ThreadSafeBlockQueueStopped;
+
+    if (alreadySuspended) {
+        return;
+    }
+
     dispatch_suspend(_serialQueue);
-    _isSuspended = YES;
+    self.currentState = ThreadSafeBlockQueueStopped;
 }
 
 #pragma mark - Queue Methods
 
 - (void)queueBlock:(void(^)(void))block {
+    [self queueBlock:block shouldReplay:YES];
+}
+
+- (void)queueBlock:(void(^)(void))block shouldReplay:(BOOL)shouldReplay {
     if (block == nil) {
         return; // return early if we weren't provided a block to run
     }
 
-    dispatch_async(self.serialQueue, block);
+    BOOL isRestarting = self.currentState == ThreadSafeBlockQueueRestarting;
+
+    if (shouldReplay) {
+        [self.blocks addObject:[block copy]];
+    }
+
+    // If we're currently in the process of restarting, then we don't need
+    // to add it to the queue, since it will already be done so as part of the
+    // restart process
+    //                               isRestarting
+    //
+    //                          yes               no
+    //                   ┌────────────────┬────────────────┐
+    //                   │                │                │
+    //                   │  add to array  │  add to array  │
+    //               yes │ ────────────── │ ────────────── │
+    //                   │       x        │     add to     │
+    // shouldReplay      │                │   gcd_queue    │
+    //                   ├────────────────┼────────────────┤
+    //                   │                │                │
+    //                   │       x        │       x        │
+    //                no │ ────────────── │ ────────────── │
+    //                   │     add to     │     add to     │
+    //                   │   gcd_queue    │   gcd_queue    │
+    //                   └────────────────┴────────────────┘
+    if (!shouldReplay || !isRestarting) {
+        dispatch_async(self.serialQueue, block);
+    }
 }
 
 - (void)enQueueAllBlocksAndRun {
-    if (self.isSuspended == YES) {
-        [self startQueue];
+    BOOL isRestarting = self.currentState == ThreadSafeBlockQueueRestarting;
+
+    if (isRestarting) {
+        return;
     }
+    
+    [self startQueue];
 }
 
 - (void)enQueueAllBlocksAndRunOnComplete:(void(^)(void))onComplete {
     if (onComplete) {
-        [self queueBlock:onComplete];
+        [self queueBlock:onComplete shouldReplay:NO];
     }
 
     [self enQueueAllBlocksAndRun];
+}
+
+#pragma mark - Restart
+
+- (void)replay {
+    BOOL alreadyRestarting = self.currentState == ThreadSafeBlockQueueRestarting;
+    if (alreadyRestarting) {
+        return;
+    }
+
+    [self suspendQueue];
+    self.currentState = ThreadSafeBlockQueueRestarting; // override the state to restarting
+
+    BOOL moreBlocksHaveBeenAdded = NO;
+    NSUInteger count = self.blocks.count - 1;
+    NSUInteger i = 0;
+
+    do {
+        while (i <= count) {
+            void (^block)(void) = [self.blocks objectAtIndex:i];
+            dispatch_async(self.serialQueue, block);
+            i++;
+        }
+
+        moreBlocksHaveBeenAdded = count < self.blocks.count - 1;
+        i = count;
+        count = self.blocks.count - 1 - count;
+    } while (moreBlocksHaveBeenAdded == YES);
+
+    [self startQueue];
 }
 
 
